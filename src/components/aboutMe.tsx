f87492422233a90f3045bebe23cfdfcd72b48063
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-11-12 13:29:27 -0300

    criacao do componente aboutMe, responsavel por conter as minhas informacoes.

---
+++ src/components/aboutMe.tsx
@@ -0,0 +1,11 @@
+import { MyTechnologies } from "./myTechnologies";
+
+export function AboutMe(){
+    return(
+        <div id="aboutMe">
+            <h2>Sobre mim</h2>
+
+            <MyTechnologies/>
+        </div>
+    )
+}
