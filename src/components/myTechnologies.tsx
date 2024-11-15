adae30d82a01e223b2aa33db96dd35dd61b486cd
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-11-15 15:36:14 -0300

    Adicao de algumas tecnologias que uso.

--- src/components/myTechnologies.tsx
+++ src/components/myTechnologies.tsx
@@ -1,9 +1,40 @@
-import reactLogo from "./assets/react.svg";
+import { Containers } from "./containers/containers.tsx";
+import typescriptLogo from "../assets/typescript.svg";
+import javascriptLogo from "../assets/javascript.svg";
+import reactLogo from "../assets/react.svg";
+import nodeLogo from "../assets/node.svg"
+import "../App.css"
 
 export function MyTechnologies(){
     return(
-        <>
-            
-        </>
+        <main>
+            <h2>Ferramentas que uso:</h2>
+
+            <div className="">
+                <Containers
+                    title="React.JS"
+                    image={reactLogo}
+                    description="Desenvolvo com react faze um ano."
+                />
+
+                <Containers
+                    title="TypeScript"
+                    image={typescriptLogo}
+                    description="Desenvolvo usando TypeScript fazem dois anos."
+                />
+
+                <Containers
+                    title="JavaScript"
+                    image={javascriptLogo}
+                    description="Desenvolvo usando JavaScript fazem três anos."
+                />
+
+                <Containers
+                    title="Node.Js"
+                    image={nodeLogo}
+                    description="Desenvolvo usando Node.JS faz um ano."
+                />
+            </div>
+        </main>
     )
 }
